f499ab9a578602dc9e5eb68e462c35437373033f
shpaginalexei <shpaginalexei@gmail.com>
2024-11-30 16:05:13 +0300

    Add try execpt

--- src/bot/database/requests/user.py
+++ src/bot/database/requests/user.py
@@ -108,49 +108,52 @@ async def check_subscriptions(bot: Bot):
         users = result.all()
         
         for user in users:
-            days_left = (user.time_sub - today).days
-            
-            if not user.reminder_sent[0]:
-                if days_left == reminder_interval:
-                    if user.balance >= conf.SUB_PRICE:
-                        await bot.send_message(user.tg_id, 
-                                               payment_tomorrow_good_message(balance=user.balance),
-                                               reply_markup=payment_keyboard(tg_id=user.tg_id))
-                    else:
-                        await bot.send_message(user.tg_id, 
-                                               payment_tomorrow_not_enough_message(balance=user.balance),
-                                               reply_markup=payment_keyboard(tg_id=user.tg_id))
-                    user.reminder_sent = [True, user.reminder_sent[1]]
-                    session.add(user)
-                    debug(f'User {user.tg_id} notified about debiting tomorrow.')
-            
-            elif not user.reminder_sent[1]:
-                if days_left <= 0:
-                    if user.balance >= conf.SUB_PRICE:
-                        balance_before = user.balance
-                        user.balance -= conf.SUB_PRICE
-                        user.time_sub += timedelta(days=conf.TIME_SUB)
-                        user.enabled = True
-                        user.reminder_sent = [False, False]
-                        payment = Payment(
-                            tg_id=user.tg_id,
-                            balance_before=balance_before,
-                            deposited=-conf.SUB_PRICE,
-                        )
-                        session.add(payment)
-                        await bot.send_message(user.tg_id, 
-                                               payment_now_good_message(balance=user.balance),
-                                               reply_markup=payment_keyboard(tg_id=user.tg_id))
-                        debug(f'User {user.tg_id} notified about debiting funds.')
-                    else:
-                        user.enabled = False
-                        user.reminder_sent = [user.reminder_sent[0], True]
-                        await disable_clients_by_tg_id(tg_id=user.tg_id)
-                        await wg.save_config()
-                        await bot.send_message(user.tg_id, 
-                                               payment_now_not_enough_message(balance=user.balance),
-                                               reply_markup=payment_keyboard(tg_id=user.tg_id))
-                        debug(f'User {user.tg_id} notified of shutdown.')
-                    session.add(user)
+            try:
+                days_left = (user.time_sub - today).days
+                
+                if not user.reminder_sent[0]:
+                    if days_left == reminder_interval:
+                        if user.balance >= conf.SUB_PRICE:
+                            await bot.send_message(user.tg_id, 
+                                                payment_tomorrow_good_message(balance=user.balance),
+                                                reply_markup=payment_keyboard(tg_id=user.tg_id))
+                        else:
+                            await bot.send_message(user.tg_id, 
+                                                payment_tomorrow_not_enough_message(balance=user.balance),
+                                                reply_markup=payment_keyboard(tg_id=user.tg_id))
+                        user.reminder_sent = [True, user.reminder_sent[1]]
+                        session.add(user)
+                        debug(f'User {user.tg_id} notified about debiting tomorrow.')
+                
+                elif not user.reminder_sent[1]:
+                    if days_left <= 0:
+                        if user.balance >= conf.SUB_PRICE:
+                            balance_before = user.balance
+                            user.balance -= conf.SUB_PRICE
+                            user.time_sub += timedelta(days=conf.TIME_SUB)
+                            user.enabled = True
+                            user.reminder_sent = [False, False]
+                            payment = Payment(
+                                tg_id=user.tg_id,
+                                balance_before=balance_before,
+                                deposited=-conf.SUB_PRICE,
+                            )
+                            session.add(payment)
+                            await bot.send_message(user.tg_id, 
+                                                payment_now_good_message(balance=user.balance),
+                                                reply_markup=payment_keyboard(tg_id=user.tg_id))
+                            debug(f'User {user.tg_id} notified about debiting funds.')
+                        else:
+                            user.enabled = False
+                            user.reminder_sent = [user.reminder_sent[0], True]
+                            await disable_clients_by_tg_id(tg_id=user.tg_id)
+                            await wg.save_config()
+                            await bot.send_message(user.tg_id, 
+                                                payment_now_not_enough_message(balance=user.balance),
+                                                reply_markup=payment_keyboard(tg_id=user.tg_id))
+                            debug(f'User {user.tg_id} notified of shutdown.')
+                        session.add(user)
+            except Exception as e:
+                debug(str(e))
         
         await session.commit()
